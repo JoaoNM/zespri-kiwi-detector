33a6908ff7abd5f1e6e4214e6adbbd922eeb1c0f
Joao Lucas NM <51080629+JoaoNM@users.noreply.github.com>
2023-03-19 13:16:49 +0800

    fix app

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from fastai.vision import *
+from fastai.vision.all import *
 import gradio as gr
 
 def is_zespri(x): return x[0].isupper()
 
 learn = load_learner('model.pkl')
 
-categories = ('zespri', 'not zespri')
+categories = ('not zespri', 'zespri')
 
 def classify_image(inp):
     pred,pred_idx,probs = learn.predict(inp)
@@ -15,5 +15,5 @@ image = gr.inputs.Image(shape=(192,192))
 label = gr.outputs.Label()
 examples = ['zespri.jpg', 'not_zespri.jpg']
 
-iface = gr.Interface(fn=greet, inputs="text", outputs="text")
+iface = gr.Interface(fn=classify_image, inputs=image, outputs=label, examples=examples)
 iface.launch()
